494a0e4b0d98e88348bd4ea1e9dfc18d9b3fc1e9
Dot <68489194+dotslashinit-sh@users.noreply.github.com>
2022-12-05 19:53:51 +0530

    New code

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Courier Service System
 
 ## Introduction
-A simple MySQL powered Courier Service system written in python.
+A simple MySQL powered Courier Service system written in python. There are way too much improvements that could be done in this program. If it looks dirty, its probably because I can't concern myself with keeping this one clean and with the features of the latest python versions. I'll definitely fix it all up when I get the time.
 
 ## How-To-Run
  - Install Python (> 3.5).
--- courier.py
+++ courier.py
@@ -2,182 +2,49 @@ import mysql.connector
 
 # con: The primary connection to the existing up and running MySQL server.
 # cur: The primary cursor used to interact with the MySQL server.
-con = mysql.connector.connect(user='root', password='root', database='courier', host='localhost')
-if not con.is_connected():
+Con = mysql.connector.connect(user='root', password='root', database='courier', host='localhost')
+if not Con.is_connected():
     print("Error connecting to MySQL database! Please check your environment setup.")
-cur = con.cursor()
+Cur = Con.cursor()
+
+# CourierCount: Used to track the count of the number of couriers and assign IDs automatically.
+# Initial value is the count of the couriers in the database.
+
+CourierCount = 0
 
 def setup():
     TABLE_BUILD_CMDS = [
-        "CREATE TABLE couriers(CourierID CHAR(5) PRIMARY KEY, FromLoc TEXT, ToLoc TEXT)",
-        "CREATE TABLE transportations(TransportID CHAR(5) PRIMARY KEY, Transport CHAR(10), Capacity INT)",
-        "CREATE TABLE shipments(TransportID CHAR(5) NOT NULL,\
-        CourierID CHAR(5) NOT NULL,\
-        ShippingTime DATE,\
-        FOREIGN KEY(TransportID) REFERENCES transportations(TransportID),\
-        FOREIGN KEY(CourierID) REFERENCES couriers(CourierID))"
+        # Users table
+        "CREATE TABLE users(Username CHAR(30) PRIMARY KEY, Password CHAR(30))",
+
+        # Couriers table
+        "CREATE TABLE couriers(CourierID CHAR(5) PRIMARY KEY, CustomerID CHAR(30) UNIQUE NOT NULL,"\
+            "FromLoc TEXT, ToLoc TEXT,"\
+            "ServiceTier CHAR(10), RequestDate DATE, FOREIGN KEY(CustomerID) REFERENCES users(Username))"
     ]
     for cmd in TABLE_BUILD_CMDS:
         try:
-            cur.execute(cmd)
-        except mysql.connector.Error:
+            Cur.execute(cmd)
+        except mysql.connector.Error as err:
+            print(err.msg)
             pass
-    cur.execute("COMMIT")
+    Con.commit()
+
+    global CourierCount
+    Cur.execute("SELECT COUNT(*) FROM couriers;")
+    CourierCount = int(Cur.fetchone()[0])
 
 def cleanup():
     TABLE_DESTROY_CMDS = [
-        "DROP TABLE shipments;",
         "DROP TABLE couriers;",
-        "DROP TABLE transportations;"
+        "DROP TABLE users;",
     ]
     for CMD in TABLE_DESTROY_CMDS:
         try:
-            cur.execute(CMD)
+            Cur.execute(CMD)
         except mysql.connector.Error:
             pass
-    cur.execute("COMMIT")
-
-def addTransportation():
-    transportID = input("Enter transport ID (5 characters): ").strip()
-    transport = input("Enter transport type (Air/Water/Land): ").strip()
-    capacity = input("Enter capacity (couriers): ").strip()
-    cur.execute("INSERT INTO transportations VALUES (%s, %s, %s)", (transportID, transport, capacity))
-    cur.execute("COMMIT")
-
-def viewTransportation(transportID = None):
-    if transportID == None:
-        transportID = input("Enter transport ID: ").strip()
-    cur.execute("SELECT * FROM transportations WHERE TransportID=%s", (transportID,))
-    data = cur.fetchone()
-    if data == None:
-        print(" No transport with the given ID!")
-    else:
-        print('#'*30, "\nTransportation details:")
-        print(" Transport ID: {}\n Transport type: {}\n Capacity (Couriers): {}".format(data[0], data[1], data[2]))
-    return data
-
-def editTransportation():
-    transportID = input("Enter transport ID: ").strip()
-    data = viewTransportation(transportID)
-    print('#' * 30)
-    if data == None:
-        return
-    (transportID, transport, capacity) = data
-    while True:
-        print("Edit transport", transportID)
-        print(" 1) Change transport ID")
-        print(" 2) Change transport type")
-        print(" 3) Change transport capacity")
-        print(" *) Continue")
-        choice = int(input("Enter your choice: ").strip())
-        if choice==1:
-            transportID = input("Enter new transport ID: ").strip()
-        elif choice==2:
-            transport = input("Enter new transport type: ").strip()
-        elif choice==3:
-            capacity = input("Enter new transport capacity: ").strip()
-        else:
-            break
-    cur.execute("UPDATE transportations SET TransportID=%s, Transport=%s, Capacity=%s", (transportID, transport, capacity))
-    cur.execute("COMMIT")
-    print("Successfully updated the data!\n")
-
-def manageTransportations():
-    while True:
-        print('#' * 30)
-        print("Manage transportations:")
-        print(" 1) Add new transportation")
-        print(" 2) View transportation details")
-        print(" 3) Edit transportation details")
-        print(" 4) Return")
-        choice = int(input("Enter your choice: ").strip())
-        if choice == 1:
-            addTransportation()
-        elif choice == 2:
-            viewTransportation()
-        elif choice == 3:
-            editTransportation()
-        elif choice==4:
-            break
-
-def addCourier():
-    courierID = input("Enter courier ID (5 characters max): ").strip()
-    fromLoc = input("Enter from location: ").strip().capitalize()
-    toLoc = input("Enter to location: ").strip().capitalize()
-    cur.execute("INSERT INTO couriers VALUES (%s, %s, %s)", (courierID, fromLoc, toLoc))
-    cur.execute("COMMIT")
-
-def findCouriers(courierID = None, fromLoc = None, toLoc = None):
-    if courierID == None and fromLoc == None and toLoc == None:
-        print("Find couriers:")
-        print(" 1) Find courier by ID")
-        print(" 2) Find courier by from location")
-        print(" 3) Find courier by to location")
-        choice = int(input("Enter your choice: ").strip())
-        if choice == 1:
-            courierID = input("Enter courier ID to find: ").strip()
-        elif choice == 2:
-            fromLoc = input("Enter from location to find: ")
-        elif choice == 3:
-            toLoc = input("Enter to location to find: ")
-        else:
-            return
-    courierID = "%" if courierID == None else courierID
-    fromLoc = "%" if fromLoc == None else fromLoc
-    toLoc = "%" if toLoc == None else toLoc
-    cur.execute("SELECT * FROM couriers WHERE \
-        CourierID LIKE %s AND FromLoc LIKE %s AND ToLoc LIKE %s", (courierID, fromLoc, toLoc))
-    data = cur.fetchall()
-    print("Search results: ")
-    if len(data) == 0:
-        print("Nothing to show :(")
-    else:
-        print("[ CourierID | From Location | To Location ]")
-        for d in data:
-            print("  {} | {} | {} ".format(d[0].rjust(9), d[1].rjust(13), d[2].rjust(11)))
-
-
-def manageCouriers():
-    while True:
-        print('\n' + '#' * 30)
-        print("Manage couriers: ")
-        print(" 1) Add courier")
-        print(" 2) Find couriers")
-        print(" 3) Return")
-        choice = int(input("Enter your choice: ").strip())
-        if choice == 1:
-            addCourier()
-        elif choice == 2: 
-            findCouriers()
-        elif choice == 3:
-            break
-
-def deployShipment():
-    print('#' * 30)
-    transportID = input("Select transportation by ID: ")
-    courierID = input("Select courier by ID: ")
-    shippingTime = input("Enter shipping time (yyyy-mm-dd format): ")
-    cur.execute("INSERT INTO shipments VALUES(%s, %s, %s)", (transportID, courierID, shippingTime))
-    cur.execute("COMMIT")
-
-def findShipment(transportID = "", courierID = "", shippingDate = ""):
-    if transportID == "" and courierID == "" and shippingDate == "":
-        transportID = input("Enter transport ID: ").strip()
-        courierID = input("Enter courierID: ").strip()
-        shippingDate = input("Enter shipping date (yyyy-mm-dd format): ").strip()
-    transportID = "%" if transportID == "" else transportID
-    courierID = "%" if courierID == "" else courierID
-    shippingDate = "%" if shippingDate == "" else shippingDate
-    cur.execute("SELECT * FROM shipments WHERE\
-        TransportID LIKE %s, CourierID LIKE %s, ShippingDate LIKE %s", (transportID, courierID, shippingDate))
-    data = cur.fetchall()
-    print("Search results: ")
-    if len(data) == 0:
-        print("Nothing to show :(")
-    else:
-        print("[ TransportID | CourierID | Shipping Date ]")
-        for d in data:
-            print("  {} | {} | {}".format(d[0].rjust(11), d[1].rjust(9), d[2].rjust(13)))
+    Con.commit()
 
 def rebuild():
     if input("Rebuilding the database destroys all the tables and rebuilds them.\n"
@@ -186,30 +53,108 @@ def rebuild():
         cleanup()
         setup()
 
+def _getUsers():
+    Cur.execute("SELECT * FROM users")
+    return Cur.fetchall()
+
+def signUp():
+    while True:
+        print('-'*30)
+        print("SIGNUP")
+        username = input("Enter your username: ").strip()
+        password = input("Enter your password: ").strip()
+        for user in _getUsers():
+            if user[0] == username:
+                print("Username already taken! Please enter a different username and try again!")
+                continue
+        Cur.execute("INSERT INTO users VALUES(%s, %s)", (username, password))
+        break
+    Con.commit()
+
+LoginInformation = None
+
+def login():
+    username = input("Enter your username: ").strip()
+    password = input("Enter your password: ").strip()
+    Cur.execute("SELECT Username FROM users WHERE Username=%s AND Password=%s", (username, password))
+    if Cur.fetchall() == []:
+        print("Invalid username/password combo!")
+    else:
+        global LoginInformation
+        print("Successfully logged in as", username, "!")
+        LoginInformation = {"Username": username, "Password": password}
+
+def sendCourier():
+    global LoginInformation, CourierCount
+    if LoginInformation == None:
+        print("You need to login before sending a courier! Please login!")
+        return
+    print("Sending courier as", LoginInformation["Username"])
+    fromLoc = input("Enter from location: ")
+    toLoc = input("Enter to location: ")
+    requestDate = input("Enter the date of request (YYYY-MM-DD format): ")
+    print("Enter the service tier you would like to opt:"\
+        " 1) Standard - Basic courier service with International shipping.",\
+        " 2) Prime - Faster delivery ( + INR 1500.00 )",\
+        " 3) PrimePlus - Fastest delivery and 5\% off of your next 3 couriers! ( + INR 2500.00 )", sep="\n")
+    serviceTierChoice = int(input("Enter your choice: ").strip())
+    serviceTier = None
+    if serviceTierChoice == 1:
+        serviceTier = "Standard"
+    elif serviceTierChoice == 2:
+        serviceTier = "Prime"
+    elif serviceTierChoice == 3:
+        serviceTier = "PrimePlus"
+    else:
+        print("Invalid service tier choice", serviceTierChoice, "! Cancelling last request!")
+        return
+    print("Selecting service tier", serviceTier)
+    CourierCount += 1
+    courierId = CourierCount
+    Cur.execute("INSERT INTO couriers VALUES(%s, %s, %s, %s, %s, %s)", \
+        (courierId, LoginInformation["Username"], fromLoc, toLoc, serviceTier, requestDate))
+    print("Successfully sent courier! Your courier ID is", courierId)
+    Con.commit()
+
+def trackCourier():
+    courierId = int(input("Enter your courier ID: ").strip())
+    Cur.execute("SELECT * FROM couriers WHERE CourierID=%s", (courierId,))
+    dat = Cur.fetchone()
+    if dat == ():
+        print("No courier with the given ID!")
+    else:
+        if dat[1] != LoginInformation["Username"]:
+            print("Mismatch between currently logged in user and the user who sent the courier!",
+            "Please login as the user who sent the courier to track it!")
+        else:
+            print(("Courier ID: {}\nService Tier: {}\nFrom location: " +\
+                "{}\nTo location: {}\nDate of sending: {}")\
+                    .format(dat[0], dat[4], dat[2], dat[3], dat[5]))
+
 #################### Main logic ####################
 
 setup()
 
 while True:
-    print('\n' + ('#' * 30))
+    print('\n', ('#' * 30))
     print("Welcome to control panel!")
-    print(" 1) Manage transportations")
-    print(" 2) Manage couriers")
-    print(" 3) Deploy shipment")
-    print(" 4) Cleanup and recreate the database")
-    print("Anything else will close the prompt.")
+    print(" 1) Sign up")
+    print(" 2) Log in")
+    print(" 3) Send a courier")
+    print(" 4) Track a courier")
+    print("Anything else closes the program.")
     choice = int(input("Enter your choice: "))
     if choice == 1:
-        manageTransportations()
+        signUp()
     elif choice == 2:
-        manageCouriers()
+        login()
     elif choice == 3:
-        deployShipment()
+        sendCourier()
     elif choice == 4:
-        rebuild()
+        trackCourier()
     else:
         break
 
-con.close()
+Con.close()
             
 ####################################################
